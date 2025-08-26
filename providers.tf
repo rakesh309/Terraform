ab512cc8e2002ef1a84bb13695d621a0dad0b838
rakesh309 <68439233+rakesh309@users.noreply.github.com>
2025-08-26 13:35:46 +0530

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,17 @@
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaz7lkdcw3rjveeybdrejtohzqolph7tp6hfuhsoj2xnjgebvnho7a"
+  user_ocid = "ocid1.user.oc1..aaaaaaaadczfcpmyadxkozitv2ae3n7pwheexop2wgyphf5rwfxikuisbfma" 
+  private_key_path = "C:\\Users\\Rakesh S\\Downloads\\terraform_1.13.0_windows_386\\oci_api_key.pem"
+  fingerprint = "69:e0:05:6b:3b:03:4f:ac:3c:f8:ad:8d:58:68:a4:53"
+  region = "ap-mumbai-1"
+  alias = "ap-mumbai-1"
+}
+
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaiypeiu2gswgt25jymyyqpbmvecm7eu267r2la3ngcfaomzgjt5da"
+  user_ocid = "ocid1.user.oc1..aaaaaaaaahdtvwwaxbeygfwg4wfladk7flw2gz2sgxmwmoiv53butzw5kw3a" 
+  private_key_path = "C:\\Users\\Rakesh S\\Downloads\\terraform_1.13.0_windows_386\\ramesh\\oci_api_key.pem"
+  fingerprint = "ff:a0:bc:82:93:ff:d0:e1:27:6a:b2:e0:c2:ea:f4:4e"
+  region = "us-phoenix-1"
+  alias = "abc"
+}
